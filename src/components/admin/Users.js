1735663d2baff5d6b8b71b9fa34871040419a725
Joram Clervius Surface <clervius@gmail.com>
2017-11-06 23:18:24 -0500

    updates

--- src/components/admin/Users.js
+++ src/components/admin/Users.js
@@ -1,22 +1,61 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import {} from '../../utils';
+import { server  } from '../../utils';
 import {} from '../../actions';
+import axios from 'axios';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
 
 class Users extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      errors: [],
+      users: []
+    }
   }
 
+  componentDidMount() {
+    const token = localStorage.getItem('x-access-token');
+    axios.get(`${server}/users/all?token=${token}`).then(res => {
+      this.setState({ users: res.data });
+    }, e => this.setState({ errors: this.state.errors.concat('Error getting user information.')}));
+
+  }
   render() {
     return (
       <div>
         this is the Users page
-
-        <table>
+        {this.state.users.length > 0 &&
+        <table  className="table table-responsive data-tbl">
+          <thead>
+            <tr>
+              <th></th>
+              <th>User</th>
+              <th>Joined</th>
+              <th>Terms</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.users.map((user, i) => {
+              return (
+                <tr key={`user-${i}`}>
+                  <td>{i + 1}</td>
+                  <td>
+                    <Link to={`/Manager/User/${user._id}`}>
+                      <strong>{user.username}</strong>{user.achievements.length > 0 && <span> · <i className="ion-ribbon-b"></i> {user.achievements[user.achievements.length - 1].name}</span>}
+                      <small>{user.email}</small>
+                    </Link>
+                  </td>
+                  <td>{moment(user.created).format('Mo DD, YYYY')}</td>
+                  <td>{user.terms.length}</td>
+                </tr>
+              )
+            })}
+          </tbody>
         </table>
+        }
       </div>
     )
   }
